d39ac774ad5d1a0c32fba0026a3e7c864578c26b
pchen <pchen.2215@gmail.com>
2026-04-08 18:50:17 -0400

    reorganize source file

--- n_watchmen_approximation.cpp
+++ n_watchmen_approximation.cpp
@@ -6,82 +6,27 @@
 #include <map>
 
 #include <CGAL/Exact_predicates_exact_constructions_kernel.h>
+#include <CGAL/Polygon_2.h>
 
 typedef CGAL::Exact_predicates_exact_constructions_kernel Kernel;
 typedef Kernel::Point_2                                   Point;
+typedef CGAL::Polygon_2<Kernel>                           Polygon;
 
 using PatrolEdge = std::pair<Point, Point>; // first == second indicates stationary guard
 using Patrol = std::vector<PatrolEdge>;
 
+// ================================================================================================
+
 // Solver function
-void find_patrols(std::vector<Patrol>& solution, const std::vector<Point>& polygon, const int n) {
-    
-    // TEMPORARY FUNCTION BODY -- AWAITING ACTUAL IMPLEMENTATION
+void find_patrols(std::vector<Patrol>& solution, const std::vector<Point>& polygon, const int n);
 
-    Patrol patrol;
-
-    PatrolEdge edge = { Point(1, 1), Point(1, 2) };
-    patrol.push_back(edge);
-
-    edge = { Point(1, 2), Point(3, 5) };
-    patrol.push_back(edge);
-
-#if false // Verification fail case - disconnected patrol
-    edge = { Point(5, 5), Point(20, 17) };
-    patrol.push_back(edge);
-#endif
-
-    solution.push_back(patrol);
-
-#if false // Verification fail case - not exactly n patrols
-    patrol.clear();
-    edge = { Point(87.5, 79.2), Point(87.5, 79.2) };
-    patrol.push_back(edge);
-
-    solution.push_back(patrol);
-#endif
-
-    patrol.clear();
-    edge = { Point(107.5, 79.2), Point(107.5, 79.2) };
-    patrol.push_back(edge);
-
-    solution.push_back(patrol);
-
-    // TEMPORARY FUNCTION BODY -- AWAITING ACTUAL IMPLEMENTATION
-
-}
+// Finds the visibility polygon from a point in the polygon.
+Polygon visibility(const Point& pt, const std::vector<Point>& polygon);
 
 // Verifies that the specified patrol is fully connected
-bool verify(const Patrol& p) {
-    if (p.size() == 1) { return true; }
-    
-    std::map<Point, std::set<Point>> graph;
-    for (const PatrolEdge& pe: p) {
-        if (pe.first == pe.second) {
-            graph[pe.first];
-            continue;
-        }
+bool verify(const Patrol& p);
 
-        graph[pe.first].insert(pe.second);
-        graph[pe.second].insert(pe.first);
-    }
-
-    std::set<Point> visited;
-    std::vector<Point> to_visit;
-    to_visit.push_back(p[0].first);
-    while (!to_visit.empty()) {
-        Point pt = to_visit.back();
-        to_visit.pop_back();
-        if (visited.find(pt) != visited.end()) { continue; }
-
-        visited.insert(pt);
-        for (const Point& next: graph[pt]) {
-            to_visit.push_back(next);
-        }
-    }
-
-    return visited.size() == graph.size();
-}
+// ================================================================================================
 
 int main(int argc, char** argv) {
     assert(argc == 4);
@@ -144,3 +89,57 @@ int main(int argc, char** argv) {
 
     return EXIT_SUCCESS;
 }
+
+// ================================================================================================
+
+void find_patrols(std::vector<Patrol>& solution, const std::vector<Point>& polygon, const int n) {
+
+    // TODO: implement
+    assert(false);
+
+}
+
+// ================================================================================================
+
+Polygon visibility(const Point& pt, const std::vector<Point>& polygon) {
+
+    // TODO: implement
+    assert(false);
+
+    return Polygon(); // temp
+}
+
+// ================================================================================================
+
+bool verify(const Patrol& p) {
+    if (p.size() == 1) { return true; }
+    
+    std::map<Point, std::set<Point>> graph;
+    for (const PatrolEdge& pe: p) {
+        if (pe.first == pe.second) {
+            graph[pe.first];
+            continue;
+        }
+
+        graph[pe.first].insert(pe.second);
+        graph[pe.second].insert(pe.first);
+    }
+
+    std::set<Point> visited;
+    std::vector<Point> to_visit;
+    to_visit.push_back(p[0].first);
+    while (!to_visit.empty()) {
+        Point pt = to_visit.back();
+        to_visit.pop_back();
+        if (visited.find(pt) != visited.end()) { continue; }
+
+        visited.insert(pt);
+        for (const Point& next: graph[pt]) {
+            to_visit.push_back(next);
+        }
+    }
+
+    return visited.size() == graph.size();
+}
+
+// ================================================================================================
